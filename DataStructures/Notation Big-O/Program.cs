ac7adce5a274618fa017abd9e0ee0dc06d54c5a3
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-05-27 23:22:54 -0300

    Added new project about graphs

---
+++ DataStructures/Notation Big-O/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Notation_Big_O
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = { "Luis", "Roberto", "Carla", "Lais", "Helena", "Cristina", "Juliano" };
+
+            for (int i = 0; i < nomes.Length; i++)
+            {
+                if (nomes[i].Equals("Lais"))
+                {
+                    Console.WriteLine($"Olá, {nomes[i]}");
+                }
+            }
+            
+        }
+    }
+}
