380cfd5b21f213ee20e3f31d7000e2c7fd4d71d5
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-05-31 18:32:40 -0300

    Starting code graph

---
+++ DataStructures/Graph/GraphRepresentation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Graph
+{
+    class GraphRepresentation
+    {
+        /**
+         * Grafos são estrutura de dados que consistem nos seguintes componentes
+         * 
+         */
+
+        // Usando de lista por 
+        static void addEgde(LinkedList<int>[] adj, int u, int v)
+        {
+            adj[u].AddLast(v);
+            adj[v].AddLast(u);
+        }
+
+        static void printGraph(LinkedList<int>[] adj)
+        {
+            for (int i = 0; i < adj.Length; i++)
+            {
+                Console.WriteLine("\nLista de vertice" + i);
+                Console.Write("Head");
+
+                foreach (var item in adj[i])
+                {
+                    Console.Write(" -> " + item);
+                }
+                Console.WriteLine();
+            }
+        }
+
+    }
+}
