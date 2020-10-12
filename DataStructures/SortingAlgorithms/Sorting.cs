816bf39c5d826bda3105a16dc2d7c262971e2186
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-10-12 19:43:48 -0300

    Added new function that implement the Shell Sort

--- DataStructures/SortingAlgorithms/Sorting.cs
+++ DataStructures/SortingAlgorithms/Sorting.cs
@@ -52,6 +52,27 @@ namespace SortingAlgorithms
             }
         }
 
+        public static void ShellSort(int[] array)
+        {
+            int gap = 1;
+            while (gap < array.Length / 3)
+            {
+                gap = 3 * gap + 1;
+            }
+
+            while (gap >= 1)
+            {
+                for (int i = gap; i < array.Length; i++)
+                {
+                    for (int j = i; j >= gap && array[j] < array[j - gap]; j -= gap)
+                    {
+                        Swap(array, j, j - gap);
+                    }
+                }
+                gap /= 3;
+            }
+        }
+
         public static void Swap(int[] array, int i, int j)
         {
             if (i == j)
