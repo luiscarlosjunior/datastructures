bdfa36b407523ba54bcfd48eb4b4248dd5ef2ade
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-10-12 21:09:10 -0300

    Added new function that implement the Quick Sort

--- DataStructures/SortingAlgorithms/Sorting.cs
+++ DataStructures/SortingAlgorithms/Sorting.cs
@@ -10,7 +10,7 @@ namespace SortingAlgorithms
     {
         public static void BubbleSort(int[] array)
         {
-            for (int partIndex = array.Length-1; partIndex > 0; --partIndex )
+            for (int partIndex = array.Length - 1; partIndex > 0; --partIndex)
             {
                 for (int i = 0; i < partIndex; i++)
                 {
@@ -35,7 +35,7 @@ namespace SortingAlgorithms
                     }
                 }
                 Swap(array, largestAt, partIndex);
-            }            
+            }
         }
 
         public static void InsertionSort(int[] array)
@@ -109,6 +109,55 @@ namespace SortingAlgorithms
                     else array[k] = aux[i++];
                 }
             }
+        }
+
+        public static void QuickSort(int[] array)
+        {
+            Sort(0, array.Length - 1);
+
+            void Sort(int low, int high)
+            {
+                if (high <= low) return;
+                int j = Partition(low, high);
+                Sort(low, j - 1);
+                Sort(j + 1, high);
+            }
+
+            int Partition(int low, int high)
+            {
+                int i = low;
+                int j = high + 1;
+
+                int pivot = array[low];
+                while (true)
+                {
+                    while (array[++i] < pivot)
+                    {
+                        if (i == high)
+                        {
+                            break;
+                        }
+                    }
+
+                    while (pivot < array[--j])
+                    {
+                        if (j == low)
+                        {
+                            break;
+                        }
+                    }
+
+                    if (i >= j)
+                    {
+                        break;
+                    }
+
+                    Swap(array, i, j);
+                }
+                Swap(array, low, j);
+                return j;
+            }
+
         }
 
         public static void Swap(int[] array, int i, int j)
