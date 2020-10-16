070f93d0157bad0578a3f4ca4e1307bcd21e2a82
luiscarlosjunior <luis.carlosjr@hotmail.com.br>
2020-10-16 20:55:11 -0300

    Added new feature

---
+++ DataStructures/List_datastructre/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace List_datastructre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node first = new Node() { Value = 5 };
+            Node second = new Node() { Value = 1 };
+
+            first.Next = second;
+
+            Node third = new Node() { Value = 3 };
+            second.Next = third;
+
+            PrintOutLinkedList(first);
+
+            Console.Read();
+        }
+
+        private static void PrintOutLinkedList(Node node)
+        {
+            while (node != null)
+            {
+                Console.WriteLine(node.Value);
+                node = node.Next;
+            }
+        }
+
+    }
+}
